dc95a1bde9a385e773142256f9b2163abe359d77
Juneyy <36472210+Juneyy@users.noreply.github.com>
2024-02-09 18:30:44 -0800

    Update load_model.py

--- load_model.py
+++ load_model.py
@@ -8,11 +8,9 @@ Original file is located at
 """
 
 import pandas as pd
-import matplotlib.pyplot as plt
 import keras
 import tensorflow as tf
 import requests
-import pandas as pd
 import numpy as np
 import csv
 
